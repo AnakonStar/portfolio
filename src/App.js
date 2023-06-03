080888cf902e936d31070c0b9ed833b157d4c263
AnakonStar <enzoh3h7@gmail.com>
2023-06-02 22:04:16 -0300

    Create the Portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
+import Header from './components/Header';
+import Lottie, {LottieRefCurrentProps} from "lottie-react";
+import animIntroduc from './assets/animations/introduc-animation.json';
 
 function App() {
+  const introRef = useRef<LottieRefCurrentProps>(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <main>
+        <div className='introduction'>
+        <Lottie className='animat' lottieRef={introRef} animationData={animIntroduc} />
+          <div>
+            <h1>
+              Portfólio de <br/>
+              <span>
+                Enzo Santana
+              </span>
+            </h1>
+            <Lottie className='animat' lottieRef={introRef} animationData={animIntroduc} />
+          </div>
+        </div>
+        <div className='certificates'>
+
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import '../App.css';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href=''>Introduction</a>
+                    </li>
+                    <li>
+                        <a href=''>Certificates</a>
+                    </li>
+                    <li>
+                        <a href=''>Projects</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
